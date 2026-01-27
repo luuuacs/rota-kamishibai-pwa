ba5954d37b53160b7c43dba08c330c66f7ce223e
Lucas Oliveira <97169513+luuuacs@users.noreply.github.com>
2026-01-27 04:12:40 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -3,7 +3,7 @@
  * Versão: 1.0.1 (Mude este número para forçar atualização nos clientes)
  */
 
-const CACHE_NAME = "rota-kamishibai-v1.0.2";
+const CACHE_NAME = "rota-kamishibai-v1.0.3";
 
 // Arquivos essenciais para funcionamento offline
 const STATIC_ASSETS = [
@@ -77,4 +77,5 @@ self.addEventListener("fetch", (event) => {
         })
     );
 });
+
 
